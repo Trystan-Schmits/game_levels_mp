338c876f1b95e92e5dedb74f36d8ef54f4dd9098
Trystan-Schmits <trystan.yt.2@gmail.com>
2024-02-27 21:57:37 +0000

    beggining setup for level 4

---
+++ assets/js/platformer2/Dog.js
@@ -0,0 +1,76 @@
+import Character from './Character.js';
+import GameEnv from './GameEnv.js';
+import playGoombaDeath from './Audio.js';
+import GameControl from './GameControl.js';
+/*
+    checklist:
+    -giant dog as boss, gets larger with difficulty: _
+    -Animation of walking in (level screen effect?): _
+    -3 hit kill (level clear condition): _
+    -death animation: _
+    -death sound (slowed down meow): _
+    -boss attacks: _
+*/
+export class Dog extends Character {
+    // constructors sets up Character object 
+    constructor(canvas, image, data, xPercentage, yPercentage, name, minPosition){
+        super(canvas, image, data, 0.0, 0.2);
+        
+        this.y = yPercentage;
+        //Initial Position of Goomba
+        this.x = xPercentage * GameEnv.innerWidth;
+
+        //Access in which a Goomba can travel    
+        this.minPosition = minPosition * GameEnv.innerWidth;
+        this.maxPosition = this.x + xPercentage * GameEnv.innerWidth;
+
+        this.immune = 0;
+
+        //Define Speed of Enemy
+        if (["easy", "normal"].includes(GameEnv.difficulty)) {
+            this.speed = this.speed * Math.floor(Math.random() * 1.5 + 2);
+        } else if (GameEnv.difficulty === "hard") {
+            this.speed = this.speed * Math.floor(Math.random() * 3 + 3);
+        } else {
+            this.speed = this.speed * 5
+        }
+    }
+
+    update() {
+        super.update();
+        
+        // Check for boundaries
+        if (this.x <= this.minPosition || (this.x + this.canvasWidth >= this.maxPosition)) {
+            this.speed = -this.speed;
+        };
+
+        // Every so often change direction
+        switch(GameEnv.difficulty) {
+            case "normal":
+                if (Math.random() < 0.005) this.speed = -this.speed;
+                break;
+            case "hard":
+                if (Math.random() < 0.01) this.speed = -this.speed;
+                break;
+            case "impossible":
+                if (Math.random() < 0.02) this.speed = -this.speed;
+                break;
+        }
+
+        // Move the enemy
+        this.x -= this.speed;
+
+        this.playerBottomCollision = false;
+    }
+    
+    // Player action on collisions
+    collisionAction() {
+        if (this.collisionData.touchPoints.other.id === "jumpPlatform") {
+            if (this.collisionData.touchPoints.other.left || this.collisionData.touchPoints.other.right) {
+                this.speed = -this.speed;            
+            }
+        }
+    }
+}
+
+export default Dog;
